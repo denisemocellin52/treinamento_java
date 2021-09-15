00fac052ba712d4e41b4badf5b693484a8610cde
drmocellin52@hotmail.com <Denise Mocellin>
2021-09-15 16:04:02 -0300

    commit aula11

--- aula11/spring02/spring02/src/main/java/com/itau/spring02/model/Usuario.java
+++ aula11/spring02/spring02/src/main/java/com/itau/spring02/model/Usuario.java
@@ -1,5 +1,50 @@
 package com.itau.spring02.model;
 
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Entity // esta classe será mapeada no Banco de Dados
+@Table(name = "tb_usuario")
 public class Usuario {
+
+    @Id // será chave primária mas não é com o id do código ID
+    // o BD gera este código automaticamente, em sequencia: 1, 2, 3...
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nome", length = 200, nullable = false)
+    private String nome;
+
+    @Column(name = "email", length = 80, nullable = false, unique = true)
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
     
 }
